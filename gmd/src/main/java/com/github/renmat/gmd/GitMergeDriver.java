6b6ecdfe2b15e619c7021dc03243a32202a8718d
dev <234611+renmat@users.noreply.github.com>
2024-05-27 20:16:48 -0400

    custom git merge driver
    https://git-scm.com/docs/gitattributes
    ​
    
    To define a custom merge driver filfre, add a section to your
    $GIT_DIR/config file (or $HOME/.gitconfig file) like this:
    
    [merge "customjsonmergedriver"]
    	name = custom json merge driver
    	driver = exec_path json %O %A %B %P
    	recursive = binary
    
    common ancestor’s version (%O), current version (%A) and the other
    branches' version (%B)
    
    specify the files for which the custom driver applies in the
    .gitattributes file
    
    *.json merge=customjsonmergedriver

--- gmd/src/main/java/com/github/renmat/gmd/GitMergeDriver.java
+++ gmd/src/main/java/com/github/renmat/gmd/GitMergeDriver.java
@@ -1,9 +1,40 @@
 package com.github.renmat.gmd;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+
 public class GitMergeDriver {
 
-	public static void main(String[] args) {
-		
+	/**
+	 * %O base version (parent of source and target)
+	 * %A current branch version (source)
+	 * %B target branch version(target)
+	 * %P file name for storing merge result
+	 * @param args extension %O %A %B %P
+	 */
+	public static void main(String[] args) throws Exception {
+		String extension = args[0];
+		Path oursPath = Paths.get(args[2]);
+		String ours = Files.readString(oursPath);
+		String thiers = Files.readString(Paths.get(args[3]));
+		if("json".equals(extension)) {
+			String merged = jsonMerge(ours, thiers);
+			Files.write(oursPath, merged.getBytes());
+		} else if("java".equals(extension)) {
+			//TODO JDT AST merge
+		}
+	}
+
+	private static String jsonMerge(String sourceJson, String targetJson) throws JsonMappingException, JsonProcessingException {
+		JsonMapper jsonMapper = JsonMapper.builder().build();
+		Object base = jsonMapper.readValue(sourceJson, Object.class);
+		Object merged = jsonMapper.readerForUpdating(base).readValue(targetJson);
+		return jsonMapper.writerWithDefaultPrettyPrinter().writeValueAsString(merged);
 	}
 
 }
